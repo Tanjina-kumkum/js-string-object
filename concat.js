c0b7f4497e68df3bf58cbfbf6041170d97c332fc
Tanjina-kumkum <tanjinaakter882@gmail.com>
2025-08-02 21:32:25 +0600

    slice and concat js added

---
+++ concat.js
@@ -0,0 +1,14 @@
+const first = "Abid";
+const last = "Nabid"
+const fullName = first + last
+console.log(fullName);
+console.log(first + ' ' + last);
+const fullName2 = first.concat(last)
+
+console.log(fullName2);
+console.log(first.concat(' ').concat(last));
+console.log(first.includes('d'));
+console.log(fullName.includes('x'));
+
+
+
---
+++ slice.js
@@ -0,0 +1,31 @@
+const address = "Kishoreganj";
+const part = address.slice(2,6)
+console.log(part);
+console.log(address.slice(3,7));
+
+const sentence = "I am a good working and hardworking person";
+const part1 = sentence.split('')
+console.log(part1);
+console.log(sentence.split(' '));
+console.log(sentence.split('a'));
+console.log(sentence.split());
+
+const friendsStr = 'Rahim,Karim,Salim,Halim';
+const friends = friendsStr.split()
+console.log(friends);
+console.log(friendsStr.split(','));
+const realFriends = ['Hasan', 'Rakib', 'Karim', 'Rahim']
+const realFriend = realFriends.join()
+console.log(realFriends);
+console.log(realFriends.join('|'));
+console.log(realFriends.join('-'));
+
+
+
+
+
+
+
+
+
+
