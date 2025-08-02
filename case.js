1aa62f047b6328283fd142884a4118dd838ae549
Tanjina-kumkum <tanjinaakter882@gmail.com>
2025-08-02 20:46:18 +0600

    case section added

--- case.js
+++ case.js
@@ -10,3 +10,29 @@ else{
     console.log('bad luck');
     
 }
+if(subject.toLowerCase() === book.toLowerCase()){
+console.log("get to good result");
+
+}
+else{console.log("bad luck");
+}
+
+const drink = ' water';
+const liquid = 'water ';
+
+if(drink === liquid){
+    console.log('water is the another name of life');
+    
+}
+else{
+    console.log('drink and liquid are the same thing but the some differ');
+    
+}
+if(drink.trim() === liquid.trim()){
+    console.log('water is the another name of life');
+    
+}
+else{
+    console.log('drink and liquid are the same thing but the some differ');
+    
+}
