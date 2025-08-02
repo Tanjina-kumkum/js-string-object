c3c082bffb90d1a22f362eff9037a8925bb8eb27
Tanjina-kumkum <tanjinaakter882@gmail.com>
2025-08-02 18:24:21 +0600

    first commit

---
+++ case.js
@@ -0,0 +1,12 @@
+const school = "RAJ UK UTTARA MODEL SCHOOL";
+console.log(school);
+const subject = 'Chemistry';
+const book = 'chemistry';
+if(subject === book){
+    console.log( 'get to good result');
+    
+}
+else{
+    console.log('bad luck');
+    
+}
---
+++ string.js
@@ -0,0 +1,29 @@
+const country = "Bangladesh";
+const division = "Sylhet";
+const district =`B-baria`;
+const thana = new String('Demra');
+console.log(thana);
+console.log(typeof thana);
+console.log(typeof district);
+console.log( typeof division);
+
+// array is mutable- can be changed
+// string is immutable -can not be changed
+
+
+const numbers = [23, 56,67, 87, 67,90];
+numbers[2]=11;
+console.log(numbers);
+
+console.log(numbers.length);
+const capital = 'Dhaka';
+console.log(capital.length);
+const capital1 = "Dha ka"
+console.log(capital1.length);
+capital[1]= "f"
+console.log(capital);
+
+
+
+
+
